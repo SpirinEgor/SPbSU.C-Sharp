c5ce4f6dc025c149462ceb9838924674e30487dd
Egor Spirin <spirin.egor@mail.ru>
2017-12-04 16:00:23 +0300

    fix travis and checker

--- RedBlackTree/tests/checker.py
+++ RedBlackTree/tests/checker.py
@@ -5,8 +5,7 @@ out_test = [line.strip() for line in open(sys.argv[1], 'r')]
 out_gold = [line.strip() for line in open(sys.argv[2], 'r')]
 if len(out_test) != len(out_gold):
     print False
-    exit(1
+    exit(0)
 for line_gold in out_gold:
     if not line_gold in out_test:
         print False
-        exit(1)
